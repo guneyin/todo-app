62f37ef07c5f47f2673571398c06e8f6538beb2b
Hüseyin Güney <guneyin@gmail.com>
2025-03-15 14:12:21 +0300

    [todo-u] todo app main page implemented
    page has two parts: input box and todo list. works with basic functionality

--- todo-ui/src/routes/+page.svelte
+++ todo-ui/src/routes/+page.svelte
@@ -1,2 +1,30 @@
 <script lang="ts">
+    let todo = $state('');
+    let todos = $state([]) as string[];
 </script>
+
+<article data-testid="add-item">
+    <fieldset role="group">
+        <input
+                id="todo-item"
+                name="todo-item"
+                type="text"
+                placeholder="Add new todo"
+                aria-label="Add new todo"
+                bind:value={todo}
+        />
+        <input type="submit" value="+Add" onclick={() => todos.push(todo)}/>
+    </fieldset>
+</article>
+
+<article data-testid="todos">
+    {#if todos.length === 0}
+        <span>No data found</span>
+    {:else}
+        <ul>
+            {#each todos as item}
+                <li>{item}</li>
+            {/each}
+        </ul>
+    {/if}
+</article>

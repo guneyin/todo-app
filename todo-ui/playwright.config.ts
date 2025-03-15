d46371a25edb3644e686bddfba267efb086415ae
Hüseyin Güney <guneyin@gmail.com>
2025-03-15 12:20:47 +0300

    [todo-ui] project template created

---
+++ todo-ui/playwright.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+	webServer: {
+		command: 'npm run build && npm run preview',
+		port: 4173
+	},
+
+	testDir: 'e2e',
+
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'], headless: false }
+		}
+	]
+});

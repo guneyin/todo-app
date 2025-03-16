8258a48f6be45065f91eabd7f004420dfdcffd1f
Hüseyin Güney <guneyin@gmail.com>
2025-03-16 13:34:24 +0300

    [todo-api] e2e test refactored

--- todo-api/e2e_test.go
+++ todo-api/e2e_test.go
@@ -2,31 +2,34 @@ package main_test
 
 import (
 	"fmt"
-	"github.com/guneyin/todo-app/todo-api/server"
+	"github.com/guneyin/todo-app/todo-api/handler"
 	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
 
 	pact "github.com/pact-foundation/pact-go/v2/provider"
-	"log"
 	"os"
 	"testing"
 )
 
-func Test_Todo(t *testing.T) {
-	go startMockServer()
+func mockServer() *httptest.Server {
+	mux := http.NewServeMux()
+	handler.RegisterRoutes(mux)
+
+	return httptest.NewServer(mux)
+}
+
+func Test_E2E(t *testing.T) {
+	ms := mockServer()
+	defer ms.Close()
 
 	var dir, _ = os.Getwd()
 	var pactDir = fmt.Sprintf("%s/../todo-ui/pacts", dir)
 
 	verifier := pact.NewVerifier()
 	err := verifier.VerifyProvider(t, pact.VerifyRequest{
-		ProviderBaseURL: "http://127.0.0.1:3000",
+		ProviderBaseURL: ms.URL,
 		PactFiles:       []string{fmt.Sprintf("%s/todo-ui-todo-api.json", pactDir)},
 	})
 	require.NoError(t, err)
-}
-
-func startMockServer() {
-	if err := server.StartServer("3000"); err != nil {
-		log.Fatal(err)
-	}
 }

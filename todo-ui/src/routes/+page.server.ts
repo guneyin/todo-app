37e9fb5fedd0c9a3b1bcdb743d54bbc6c49052cc
Hüseyin Güney <guneyin@gmail.com>
2025-03-16 18:41:13 +0300

    [todo-ui] error handling enhanced
    api errors handled

--- todo-ui/src/routes/+page.server.ts
+++ todo-ui/src/routes/+page.server.ts
@@ -1,12 +1,20 @@
 import type { Actions, PageServerLoad } from './$types';
 import { env } from '$env/dynamic/public';
+import { fail } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async () => {
 	let todos: string[] = [];
 
 	const fetched = await fetch(`http://${env.PUBLIC_API_URL}/api/todos`)
-		.then((res) => res.json())
-		.catch((err) => console.log(err));
+		.then(async (r) => {
+			if (!r.ok) {
+				const body = await r.text();
+				return fail(r.status, { error: body });
+			}
+
+			return await r.json();
+		})
+		.catch((err) => fail(500, { error: err.error }));
 
 	if (fetched) {
 		todos = fetched.todos;
@@ -21,14 +29,19 @@ export const actions: Actions = {
 		const todo = data.get('todo-item');
 
 		const body = JSON.stringify({ todo: todo });
-		await fetch(`http://${env.PUBLIC_API_URL}/api/todos`, {
+		return await fetch(`http://${env.PUBLIC_API_URL}/api/todos`, {
 			method: 'POST',
 			body: body,
 			headers: {
 				'Content-Type': 'application/json'
 			}
-		}).catch((err) => {
-			throw err;
-		});
+		})
+			.then(async (r) => {
+				if (!r.ok) {
+					const body = await r.text();
+					return fail(r.status, { error: body });
+				}
+			})
+			.catch((err) => fail(500, { error: err.error }));
 	}
 };
